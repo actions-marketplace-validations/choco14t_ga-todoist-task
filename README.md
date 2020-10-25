0ae93840087164575531611f7c45444d65cf860a
choco <chocoh14t@gmail.com>
2020-10-25 10:41:24 +0900

    :sparkles: initial commit

---
+++ README.md
@@ -0,0 +1,29 @@
+# Create Todoist Task Action
+
+This action creates a Todoist task with GitHub Actions.
+
+## Inputs
+
+### `token`
+
+**Required** The value of Todoist API token. Default `null`.
+
+[Check your API token](https://todoist.com/Users/viewPrefs?page=integrations)
+
+### `project-name`
+
+The project name you want to add a task.
+
+**Note** Search project name with exact match.
+
+## Outputs
+
+### `message`
+
+The response of Todoist API created task.
+
+[More detail](https://developer.todoist.com/rest/v1/?shell#create-a-new-task)
+
+## Example usage
+
+WIP...
---
+++ index.js
@@ -0,0 +1,52 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+const axios = require('axios');
+
+const BASE_URI = 'https://api.todoist.com/rest/v1';
+const http = axios.create({
+  baseURL: BASE_URI,
+  headers: {
+    Authorization: `Bearer ${core.getInput('token')}`,
+  },
+});
+
+const fetchProjectIdFromName = async (name) => {
+  const response = await http.get('/projects');
+
+  const projects = response.data;
+  const project = projects.filter((project) => project.name === name).pop();
+
+  return project ? project.id : null;
+};
+
+const createTask = async ({ content, projectId }) => {
+  const body = {
+    content,
+    project_id: projectId,
+  };
+  const response = await http.post('/tasks', body);
+
+  return response.data;
+};
+
+const run = async () => {
+  try {
+    const { context } = github;
+
+    if (context.eventName !== 'issues') {
+      throw new Error('Supported event issues only.');
+    }
+
+    const id = await fetchProjectIdFromName(core.getInput('project-name'));
+
+    const { issue } = context.payload;
+    const content = `#${issue.number} ${issue.title}`;
+    const task = await createTask({ content, id });
+
+    core.setOutput('message', JSON.stringify(task, null, 2));
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+};
+
+run();
